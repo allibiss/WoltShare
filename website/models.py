28203f9d21bc30094af1288cd16674a9407ee8d1
MarcoDiFrancesco <marcodifran@gmail.com>
2022-11-05 13:35:01 +0200

    Add user model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.CharField("Email", max_length=256)
+    password = models.CharField("Password", max_length=256)
+    name = models.CharField("Name Surname", max_length=256)
+    phone = models.CharField("Phone number", max_length=256, blank=True)
+    address = models.CharField("Address", max_length=256)
